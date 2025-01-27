85192eb423123a67c7f0b090ba2b8ed2c2bdaa28
Tex Bonham <texwasbad@gmail.com>
2025-01-26 19:52:23 -0800

    Files for My Custom Computer Architecture
    Assembly documentation lost

---
+++ MyAssembly+Emulator/Instruction.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+//using System.Collections.Generic;
+
+namespace projectclass
+{
+    public abstract class Instruction
+    {
+        private uint InstructionData => MachineCode;
+        public uint MachineCode { get; set; }
+        public string pattern { get; }
+        public byte OpCode => (byte)(MachineCode >> 24);
+        private static byte NthByte(uint number, int bitIndex)
+        {
+            number >>= (bitIndex * 8);
+            number = number & 255;
+            return (byte)number;
+        }
+        public static uint[] Parse(string[] instruction, Dictionary<string, uint> dict)
+        {
+            Dictionary<string, byte> labeldictionary = new Dictionary<string, byte>();
+            for (int i = 0; i < instruction.Length; i++)
+            {
+                if (instruction[i][instruction[i].Length - 1] == ':')
+                {
+                    labeldictionary.Add(instruction[i], (byte)i);
+                    instruction[i] = "NOP 00 00 00";
+                }
+
+            }
+
+
+
+
+            uint[] machinecodes = new uint[instruction.Length];
+            for (int i = 0; i < instruction.Length; i++)
+            {
+                Match[] matches = new Match[]
+                {
+
+                 Regex.Match(input: instruction[i], pattern: "(ADD|SUB|DIV|MULT|MOD|AND|OR|XOR|GTE|LTE|EQ) R([0-9a-fA-F]+) R([0-9a-fA-F]+) R([0-9a-fA-F]+)"),
+                 Regex.Match(input: instruction[i], pattern: "(COPY|LOAD|STORE|SHR|SHL|NOT) R([0-9a-fA-F]+) R([0-9a-fA-F]+) ([0][0])"),
+                 Regex.Match(input: instruction[i], pattern: "(SET) R([0-9a-fA-F]+) ([0-9A-F][0-9A-F]) ([0][0])"),
+                 Regex.Match(input: instruction[i], pattern: "(JMPT|JMPZ) (\\S+:) R([0-9a-fA-F]+) ([0][0])"),
+                 Regex.Match(input: instruction[i], pattern: "(POP|PUSH) R([0-9a-fA-F]+) ([0][0]) ([0][0])"),
+                 Regex.Match(input: instruction[i], pattern: "(JMP) (\\S+:) ([0][0]) ([0][0])"),
+                 Regex.Match(input: instruction[i], pattern: "(NOP) ([0][0]) ([0][0]) ([0][0])"),
+
+
+                 };
+                foreach (Match match in matches)
+                {
+                    if (match.Success)
+                    {
+                        string[] match1 = new string[4];
+                        for (int j = 0; j < match1.Length; j++)
+                        {
+                            match1[j] = match.Groups[j + 1].Value;
+                        }
+
+                        uint program = 0;
+                        program += dict[match.Groups[1].Value];
+
+                        if (match.Groups[1].Value == "JMP" || match.Groups[1].Value == "JMPT" || match.Groups[1].Value == "JMPZ")
+                        {
+                            program <<= 8;
+                            program += labeldictionary[match1[1]];
+                        }
+                        else
+                        {
+                            program <<= 8;
+                            program += uint.Parse(match1[1], System.Globalization.NumberStyles.HexNumber);
+                        }
+                        for (int k = 2; k < match1.Length; k++)
+                        {
+                            program <<= 8;
+                            program += uint.Parse(match1[k], System.Globalization.NumberStyles.HexNumber);
+                        }
+                        machinecodes[i] = program;
+                    }
+                    ;
+                }
+
+            }
+            return machinecodes;
+            throw new Exception("Wrong Syntax");
+        }
+        public static string[] UnParse(uint[] machinecode, Dictionary<uint, string> dict, Dictionary<string, uint> dict2)
+        {
+            Dictionary<int, string> labeldictionary = new Dictionary<int, string>();
+            string[] words = new string[machinecode.Length];
+            int counter = 0;
+            for (int k = 0; k < machinecode.Length; k++)
+            {
+                if (!(words[k] == "" || words[k] == null))
+                {
+                    continue;
+                }
+                
+                byte[] parts = new byte[4];
+
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    parts[3 - i] = NthByte(machinecode[k], i);
+                }
+
+                string word = dict[parts[0]];
+                int startpoint = 1;
+                if (word == "JMP" || word == "JMPT" || word == "JMPZ")
+                {
+                    word += ' ';
+                    if (labeldictionary.ContainsKey(parts[1]))
+                    {
+                        word += labeldictionary[parts[1]];
+                    }
+                    else
+                    {
+                        string temp = "";
+                      
+                        temp += 'L';
+                        temp += counter.ToString();
+                        temp += ':';
+                        word += temp;
+                        counter++;
+                        words[parts[1]] = temp;
+                        labeldictionary.Add(parts[1], temp);
+                    }
+
+                    startpoint++;
+                }
+                for (int i = startpoint; i < dict2[dict[parts[0]]] + 1; i++)
+                {
+                    word += ' ';
+                    word += 'R';
+                    word += parts[i].ToString("x2");
+                }
+                for (int i = (int)dict2[dict[parts[0]]] + 1; i < parts.Length; i++)
+                {
+                    word += ' ';
+
+                    word += parts[i].ToString("x2");
+                }
+                words[k] = word;
+            }
+
+            return words;
+        }
+    }
+}
---
+++ MyAssembly+Emulator/Program.cs
@@ -0,0 +1,528 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+namespace projectclass
+{
+    public class Animal
+    {
+        
+        public Animal()
+        {
+            Type type = GetType();
+            Type type2 = typeof(Animal);
+            if(type.IsSubclassOf(type2) && type.GetMethods(System.Reflection.BindingFlags.Public|System.Reflection.BindingFlags.Instance| System.Reflection.BindingFlags.DeclaredOnly).Length<2)
+            {
+                throw new Exception("Not Enough Methods");
+            }
+            
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public int Age;
+        public string Name;
+
+        public void PrintName()
+        {
+            Console.WriteLine(Name);
+        }
+        public void PrintAge()
+        {
+            Console.WriteLine(Age);
+        }
+    }
+
+
+    class LLN
+    {
+        public int value { get; set; }
+        public LLN Next { get; set; }
+        public static LLN Empty = new LLN(0);
+        public LLN(int Value, LLN next = null)
+        {
+            value = Value;
+            Next = next;
+        }
+    }
+    class Program
+    {
+        enum OpCodes : uint
+        {
+            //Memory
+            POP = 0x45,
+            PUSH = 0x44,
+            STORE = 0x43,
+            LOAD = 0x42,
+            COPY = 0x41,
+            SET = 0x40,
+            //Control
+            JMPZ = 0x32,
+            JMPT = 0x31,
+            JMP = 0x30,
+            //Logic
+            EQ = 0x26,
+            LSEQ = 0x25,
+            GREQ = 0x24,
+            NOT = 0x23,
+            XOR = 0x22,
+            OR = 0x21,
+            AND = 0x20,
+            // CS Math
+            SFTL = 0x16,
+            SFTR = 0x15,
+            MOD = 0x14,
+            DIV = 0x13,
+            MLT = 0x12,
+            SUB = 0x11,
+            ADD = 0x10,
+
+            //NULL
+            NOP = 0x00,
+        }
+
+        static byte[] Registers = new byte[32];
+        static byte[] memory = new byte[256];
+        static int[] Instructions = new int[10];
+
+        static int instrutionpointer = 31;
+        static int Stackpointer = 31;
+        static Stack<byte> stack = new Stack<byte>();
+        public static Dictionary<string, uint> adictionary = new Dictionary<string, uint>()
+        {
+            ["NOP"] = 0x00,
+            ["ADD"] = 0x10,
+            ["SUB"] = 0x11,
+            ["MULT"] =0x12,
+            ["DIV"]  =0x13,
+            ["MOD"]  =0x14,
+            ["SHR"]  =0x15,
+            ["SHL"]  =0x16,
+            ["AND"]  =0x20,
+            ["OR"]   =0x21,
+            ["XOR"] = 0x22,
+            ["NOT"] = 0x23,
+            ["GTE"] = 0x24,
+            ["LTE"] = 0x25,
+            ["EQ"]  = 0x26,
+            ["JMP"] = 0X30,
+            ["JMPT"] = 0X31,
+            ["JMPZ"] = 0X32,
+            ["SET"] = 0x40,
+            ["COPY"] =0x41,
+            ["LOAD"] =0x42,
+            ["STORE"]=0X43,
+            ["PUSH"] = 0X44,
+            ["POP"] = 0X45,
+        };
+        public static Dictionary<string, uint> registercountdictionary = new Dictionary<string, uint>()
+        {
+            ["NOP"] = 0,
+            ["ADD"] = 3,
+            ["SUB"] = 3,
+            ["MULT"] =3,
+            ["DIV"] = 3,
+            ["MOD"] = 3,
+            ["SHR"] = 2,
+            ["SHL"] = 2,
+            ["AND"] = 3,
+            ["OR"] =  3,
+            ["XOR"] = 3,
+            ["NOT"] = 3,
+            ["GTE"] = 3,
+            ["LTE"] = 3,
+            ["EQ"] = 3,
+            ["JMP"] = 1,
+            ["JMPT"] = 2,
+            ["JMPZ"] = 2,
+            ["SET"] = 1,
+            ["COPY"] = 2,
+            ["LOAD"] = 2,
+            ["STORE"] = 2,
+            ["PUSH"] = 1,
+            ["POP"] = 1,
+        };
+        public static Dictionary<uint, string> ddictionary = new Dictionary<uint, string>();
+        
+
+        Instruction[] GetInstructions()
+        {
+            Type type = typeof(Instruction);
+
+            // Instruction[] instructions =
+            return null;
+        }
+        static string[] intarrtostringarr(uint[] array)
+        {
+            string[] temp = new string[array.Length];
+            for(int i=0; i<array.Length;i++)
+            {
+                temp[i] = array[i].ToString("x8");
+            }
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            foreach (KeyValuePair<string, uint> i in adictionary)
+            {
+                ddictionary.Add(i.Value,i.Key);
+            }
+            string[] instructions = LoadAssemblyProgram();
+            uint[] assemblycode = new uint[instructions.Length];
+            string[] instructions2 = new string[assemblycode.Length];
+          
+                assemblycode = Instruction.Parse(instructions,adictionary);
+           
+            ;
+            
+                instructions2 = Instruction.UnParse(assemblycode, ddictionary,registercountdictionary);
+
+            ;
+            File.WriteAllLines("starttext.txt", intarrtostringarr(assemblycode));
+             assemblycode = Instruction.Parse(instructions2, adictionary);
+            File.WriteAllLines("finaltext.txt", intarrtostringarr(assemblycode));
+            ;
+            Registers[instrutionpointer] = 0;
+            while (Registers[instrutionpointer] < assemblycode.Length)
+            {
+                DoWork(assemblycode[Registers[instrutionpointer]]);
+                Registers[instrutionpointer]++;
+            }
+            ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine();
+        }
+        string disassembler(uint mcode)
+        {
+            StringBuilder newstring = new StringBuilder();
+            if ((mcode >> 24) == 0x10)
+            {
+                newstring.Append("ADD ");
+                for (int i = 0; i < 3; i++)
+                {
+                    newstring.Append("R");
+                    newstring.Append(mcode >> (8 * (2 - i)));
+                    newstring.Append(" ");
+                }
+            }
+            return newstring.ToString();
+        }
+        //Assembler----------------------------
+       
+        static string[] LoadAssemblyProgram()
+        {
+            return System.IO.File.ReadAllLines("Program.asm");
+
+        }
+        //Assembler----------------------------
+        static void Print(LLN head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.value);
+                Console.Write("->");
+                head = head.Next;
+            }
+            Console.WriteLine();
+        }
+        static unsafe void LoopThrough(int[] array, int start, int end)
+        {
+            int counter = 0;
+            fixed (int* ptr = array)
+            {
+                while (counter < end)
+                {
+                    Console.WriteLine(*(ptr + start + counter));
+                    counter++;
+                }
+            }
+        }
+        static void Subtraction(int val1, int val2)
+        {
+            val2 = (~val2) + 1;
+            val1 += val2;
+        }
+        static LLN ADDLL(LLN head1, LLN head2)
+        {
+            LLN newhead = LLN.Empty;
+            LLN nexttemp = newhead;
+            int carry = 0;
+            while (!(head1 == LLN.Empty && head2 == LLN.Empty) || carry != 0)
+            {
+                int temp = 0;
+                if (head1 == LLN.Empty && head2 == LLN.Empty)
+                {
+                    temp = carry;
+                }
+                else if (head1 == LLN.Empty)
+                {
+                    temp = head2.value + carry;
+                    head2 = head2.Next;
+                }
+                else if (head2 == LLN.Empty)
+                {
+                    temp = head1.value + carry;
+                    head1 = head1.Next;
+                }
+                else
+                {
+                    temp = head2.value + head1.value + carry;
+                    head1 = head1.Next;
+                    head2 = head2.Next;
+                }
+                if (temp >= 2)
+                {
+                    temp -= 2;
+                    carry = 1;
+                }
+                else
+                {
+                    carry = 0;
+                }
+                nexttemp.value = temp;
+                nexttemp.Next = LLN.Empty;
+                nexttemp = nexttemp.Next;
+
+
+            }
+            nexttemp = null;
+            return newhead;
+
+        }
+        static unsafe void Bubble(int[] array, int start, int end)
+        {
+            bool SortDone = false;
+
+            fixed (int* ptr = array)
+            {
+                while (!SortDone)
+                {
+                    SortDone = true;
+                    int counter = 1;
+                    while (counter < end)
+                    {
+                        if (ptr[start + counter] < ptr[start + counter - 1])
+                        {
+                            int temp = ptr[start + counter];
+                            ptr[start + counter] = ptr[start + counter - 1];
+                            ptr[start + counter - 1] = temp;
+                            SortDone = false;
+                        }
+                        counter++;
+                    }
+                }
+
+            }
+        }
+        static bool PowerofTwo(int number)
+        {
+
+            bool temp = true;
+            while (number > 1)
+            {
+                if (number != number >> 1 << 1)
+                {
+                    temp = false;
+                }
+                number >>= 1;
+            }
+            return temp;
+        }
+        static int ClosestPoweroftwo(int number)
+        {
+            return 2;
+        }
+        static bool IsNthBitOne(int number, int bitIndex)
+        {
+            number >>= bitIndex;
+            number = number & 1;
+            return number == 1;
+        }
+        static byte NthByte(uint number, int bitIndex)
+        {
+            number >>= (bitIndex * 8);
+            number = number & 255;
+            return (byte)number;
+        }
+        static int NthFlip(int number, int byteindex)
+        {
+            if (IsNthBitOne(number, byteindex))
+            {
+                return number - (int)Math.Pow(2, byteindex);
+            }
+            return number + (int)Math.Pow(2, byteindex);
+        }
+        static void DoWork(uint input)
+        {
+            uint opperator = NthByte(input, 3);
+            byte val1 = NthByte(input, 2);
+            byte val2 = NthByte(input, 1);
+            byte val3 = NthByte(input, 0);
+
+            switch ((OpCodes)opperator)
+            {
+                case OpCodes.POP:
+                    Registers[val1]=stack.Pop();
+                    break;
+                case OpCodes.PUSH:
+                    stack.Push(Registers[val1]);
+                    break;
+                case OpCodes.STORE:
+                    memory[Registers[val2]] = Registers[val1];
+                    break;
+                case OpCodes.LOAD:
+                    Registers[val1]=memory[val2];
+                    break;
+                case OpCodes.COPY:
+                    Registers[val2] = Registers[val1];
+                    break;
+                case OpCodes.SET:
+                    Registers[val1] = (byte)val2;
+                    break;
+                case OpCodes.JMPZ:
+                    if (Registers[val2] == 0)
+                    {
+                        Registers[instrutionpointer] = val1;
+                    }
+                    break;
+                case OpCodes.JMPT:
+                    if (Registers[val2] == 1)
+                    {
+                        Registers[instrutionpointer] = val1;
+                    }
+                    break;
+                case OpCodes.JMP:
+                    Registers[instrutionpointer] = val1;
+                    break;
+                case OpCodes.EQ:
+                    if (Registers[val1] == Registers[val2])
+                    {
+                        Registers[val3] = 1;
+                    }
+                    else
+                    {
+                        Registers[val3] = 0;
+                    }
+                    break;
+                case OpCodes.LSEQ:
+                    if (Registers[val1] <= Registers[val2])
+                    {
+                        Registers[val3] = 1;
+                    }
+                    else
+                    {
+                        Registers[val3] = 0;
+                    }
+                    break;
+                case OpCodes.GREQ:
+                    if (Registers[val1] >= Registers[val2])
+                    {
+                        Registers[val3] = 1;
+                    }
+                    else
+                    {
+                        Registers[val3] = 0;
+                    }
+                    break;
+                case OpCodes.NOT:
+                    if (Registers[val1] != 1 )
+                    {
+                        Registers[val2] = 1;
+                    }
+                    break;
+                case OpCodes.XOR:
+                    if (Registers[val2] != 1 && Registers[val1] != 1)
+                    {
+                        Registers[val3] = 1;
+                    }
+                    break;
+                case OpCodes.OR:
+                    if (Registers[val2] == 1 || Registers[val1] == 1)
+                    {
+                        Registers[val3] = 1;
+                    }
+                    break;
+                case OpCodes.AND:
+                    if (Registers[val2] ==1&& Registers[val1]==1)
+                    {
+                        Registers[val3] = 1;
+                    }
+                    break;
+                case OpCodes.SFTL:
+                    Registers[val2] = Registers[val1] <<= 1;
+                    break;
+                case OpCodes.SFTR:
+                    Registers[val2] = Registers[val1] >>= 1;
+                    break;
+                case OpCodes.MOD:
+                    Registers[val3] = (byte)(Registers[val1] % Registers[val2]);
+                    break;
+                case OpCodes.DIV:
+                    Registers[val3] = (byte)(Registers[val1]/Registers[val2]);
+                    break;
+                case OpCodes.MLT:
+                    int counter = 0;
+                Loop:
+                    Registers[val3] = (byte)(Registers[val1] + Registers[val3]);
+                    counter++;
+                    if (counter < val2)
+                    {
+                        goto Loop;
+                    }
+                    break;
+                case OpCodes.SUB:
+                    Registers[val3] = (byte)(Registers[val1] - (byte)Registers[2]);
+                    break;
+                case OpCodes.ADD:
+                    Registers[val3] = (byte)(Registers[val1] + (byte)Registers[val2]);
+                    break;
+                case OpCodes.NOP:
+                    break;
+            }
+        }
+        static void Loop(int times)
+        {
+            int X = 0;
+        ReLoop:
+            if (X != times)
+            {
+                X++;
+                goto ReLoop;
+            }
+
+        }
+        /*
+         * SET R1 0 
+         * SET R2 1
+         * Lb1:
+         * ADD R1 R2 R1
+         * JMP Lb1
+         * 0x40000000
+         * 0x40010001
+         * 
+         * 0x10000100
+         * 0x30FF0002
+         * Destination,Source,Source
+        */
+
+    }
+}
